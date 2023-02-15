0f8f6462e66af75c3d7d617aa0357656a99bc063
George Dziouba <george.dziouba@accenture.com>
2023-02-15 16:11:40 -0500

    Bad commit, pushing to use code later

--- Backend/WebApplication1/Controllers/DepartmentController.cs
+++ Backend/WebApplication1/Controllers/DepartmentController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using System.Data.SqlClient;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -33,6 +34,35 @@ namespace WebApplication1.Controllers
                 myCon.Open();
 
                 using(SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department department)
+        {
+            string query = @"
+                insert into dbo.Department
+                values (@Department)
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
                 {
                     myReader = myCommand.ExecuteReader();
                     table.Load(myReader);
